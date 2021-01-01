b2ddf408483faa0910465b6668528b05334e4e17
haxscramper <haxscramper@gmail.com>
2021-01-02 01:42:44 +0300

    [CHECKPOINT]

--- tests/tCommonParsers.nim
+++ tests/tCommonParsers.nim
@@ -1,9 +1,9 @@
 import std/[sugar, strutils, sequtils, strformat, unittest]
 import htsparse/[
-  toml/toml,
-  cpp/cpp,
-  java/java,
-  html/html,
+  # toml/toml,
+  # cpp/cpp,
+  # java/java,
+  # html/html,
   latex/latex
 ]
 
@@ -11,50 +11,50 @@ import htsparse/[
 # {.passl: "-lstdc++".}
 
 suite "Main":
-  test "toml":
-    let parser = newTomlParser()
-    let str = """
-[servers]
+#   test "toml":
+#     let parser = newTomlParser()
+#     let str = """
+# [servers]
 
-  # Indentation (tabs and/or spaces) is allowed but not required
-  [servers.alpha]
-  ip = "10.0.0.1"
-  dc = "eqdc10"
-"""
-    echo parser.parseString(str).treeRepr(str)
+#   # Indentation (tabs and/or spaces) is allowed but not required
+#   [servers.alpha]
+#   ip = "10.0.0.1"
+#   dc = "eqdc10"
+# """
+#     echo parser.parseString(str).treeRepr(str)
 
-  test "cpp":
-    let parser = newCppParser()
-    let str = """
-int main() {}
-"""
-    echo parser.parseString(str).treeRepr(str)
-    echo parser.parseString(str).treeRepr(str, true)
+#   test "cpp":
+#     let parser = newCppParser()
+#     let str = """
+# int main() {}
+# """
+#     echo parser.parseString(str).treeRepr(str)
+#     echo parser.parseString(str).treeRepr(str, true)
 
-  test "java":
-    let parser = newJavaParser()
-    let str = """
-class HelloWorld {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-    }
-}
-"""
-    echo parser.parseString(str).treeRepr(str)
+#   test "java":
+#     let parser = newJavaParser()
+#     let str = """
+# class HelloWorld {
+#     public static void main(String[] args) {
+#         System.out.println("Hello World!");
+#     }
+# }
+# """
+#     echo parser.parseString(str).treeRepr(str)
 
-  test "html":
-    let parser = newHtmlParser()
-    let str = """
-<body>
+#   test "html":
+#     let parser = newHtmlParser()
+#     let str = """
+# <body>
 
-<h2>HTML Links</h2>
-<p>HTML links are defined with the a tag:</p>
+# <h2>HTML Links</h2>
+# <p>HTML links are defined with the a tag:</p>
 
-<a href="https://www.w3schools.com">This is a link</a>
+# <a href="https://www.w3schools.com">This is a link</a>
 
-</body>
-"""
-    echo parser.parseString(str).treeRepr(str)
+# </body>
+# """
+#     echo parser.parseString(str).treeRepr(str)
 
   test "latex":
     let parser = newLatexParser()
